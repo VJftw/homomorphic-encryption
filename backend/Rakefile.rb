6af12cad1e6f564514294e7f483e85d35c7427d8
VJ Patel <meetthevj@gmail.com>
2016-02-02 15:01:55 +0000

    Made Rake tasks fancier

--- backend/Rakefile.rb
+++ backend/Rakefile.rb
@@ -1,44 +1,92 @@
-container_name = 'homomorphic-encryption-backend'
-prod_container_name = 'tutum.co/vjftw/homomorphic-encryption:backend-latest'
-
+# Tag structure:
+# tutum.co/vjftw/homomorphic-encryption:backend-<branch> for latest production
+# tutum.co/vjftw/homomorphic-encryption:backend-<branch>-commit
+# tutum.co/vjftw/homomorphic-encryption:backend-<branch>-dev for development
+#
 if ENV.include? 'CI' and ENV['CI'] == 'true'
   IS_CI = true
-  puts 'Continuous Integration environment'
+  puts "# Continuous Integration environment\n\n"
 else
   IS_CI = false
-  puts 'Development environment'
+  puts "# Development environment\n\n"
+end
+
+def system_command(command, silent=false, show_warnings=true, indent_amount=0)
+  output = []
+  indent = ''
+  indent_amount.times do
+    indent += "\t"
+  end
+  IO.popen(command) do |io|
+    while line = io.gets
+      unless silent
+        puts "#{indent}#{line.chomp}"
+      end
+      output << line.chomp
+    end
+    io.close
+
+    if $?.to_i != 0
+      if show_warnings
+        puts "Warning: #{command} returned: #{$?.to_i}"
+      end
+      return false
+    end
+  end
+
+  output
+end
+
+def get_current_branch
+  system_command('git rev-parse --abbrev-ref HEAD', true)[0].strip()
+end
+
+def get_current_commit
+  system_command('git describe --tags', true)[0].strip()
+end
+
+container_name = "tutum.co/vjftw/homomorphic-encryption:backend-#{get_current_branch}"
+dev_container_name = "#{container_name}-dev"
+prod_container_name = "#{container_name}-#{get_current_commit}"
+
+puts "# Container Tags\n\tProduction:\t#{container_name}\n\tDevelopment:\t#{dev_container_name} \n\tBuild:\t\t#{prod_container_name}\n\n"
+
+
+def get_dev_container(tag)
+  puts '# Looking for Development container'
+  found = system_command("docker images #{tag} | grep B", false, false, 1)
+
+  unless found
+    puts '## Building Development container'
+    build_container("#{Dir.getwd}/Dockerfile.dev", "#{Dir.getwd}", "#{tag}")
+  end
+
 end
 
 desc 'Run Tests'
 task :test do
   
-  puts 'Looking for dev image'
-  found = system_command("docker images #{container_name}:dev | grep B")
-
-  unless found
-    puts 'Building dev image'
-    build_container("#{Dir.getwd}/Dockerfile.dev", "#{Dir.getwd}", "#{container_name}:dev")
-  end
+  get_dev_container dev_container_name
 
   # start container
-  puts 'Starting dev container'
-  start_command = "docker run -d -v #{Dir.getwd}:/app #{container_name}:dev"
-  container_id = system_command(start_command)[0].strip()
+  puts '# Starting Development container'
+  start_command = "docker run -d -v #{Dir.getwd}:/app #{dev_container_name}"
+  container_id = system_command(start_command, false, true, 1)[0].strip()
 
-  # exec tests
-  puts 'Running tests'
-  nosetests = 'nosetests --rednose --force-color --with-coverage --cover-html --cover-html-dir=coverage --all-modules --cover-package=HomomorphicEncryptionBackend tests/ -v'
+  puts "\n"
 
   user = IS_CI ? 'root': 'app'
+
+  puts '# Running tests'
+  nosetests = 'nosetests --rednose --force-color --with-coverage --cover-html --cover-html-dir=coverage --all-modules --cover-package=HomomorphicEncryptionBackend tests/ -v'
+
   test_command = "docker exec -u #{user} -t #{container_id} #{nosetests}"
 
-  test_result = system_command(test_command)
-  
-  # stop and remove container
-  puts 'Stopping dev container'
-  system_command("docker stop #{container_id}")
-  puts 'Removing dev container'
-  system_command("docker rm #{container_id}")
+  test_result = system_command(test_command, false, true, 1)
+
+  puts '# Stopping and Removing Development container'
+  system_command("docker stop #{container_id}", false, true, 1)
+  system_command("docker rm #{container_id}", false, true, 1)
 
   fail 'Tests failed' unless test_result
 end
@@ -65,7 +113,12 @@ end
 
 desc 'Build production container'
 task :build_prod do
+  puts '# Building Production container'
   built_container = build_container("#{Dir.getwd}/Dockerfile.prod", "#{Dir.getwd}", prod_container_name)
+
+  puts "# Tagging as #{container_name}"
+  tag_command = "docker tag -f #{built_container} #{container_name}"
+  system_command(tag_command)
 end
 
 desc 'CI'
@@ -83,13 +136,16 @@ task :ci do
 
   docker_push = "docker push #{prod_container_name}"
   system_command(docker_push)
+
+  docker_push = "docker push #{container_name}"
+  system_command(docker_push)
 end
 
 
 def build_container(docker_file, working_dir, tag='test')
 
   command = "docker build -f #{docker_file} -t #{tag} #{working_dir}"
-  output = system_command(command, "Failed to build container for #{docker_file}")
+  output = system_command(command)
 
   output_match = /(Successfully built )(.*)/.match(output.last)
   fail 'Docker failed to build the container!' unless output_match
@@ -97,23 +153,4 @@ def build_container(docker_file, working_dir, tag='test')
   puts "Built Container Image: #{output_match[2]}"
 
   output_match[2]
-end
-
-def system_command(command, failure_message="#{command} failed.")
-  output = []
-  IO.popen(command) do |io|
-    while line = io.gets
-      puts line.chomp
-      output << line.chomp
-    end
-    io.close
-
-    if $?.to_i != 0
-      puts "Error! #{command} returned: #{$?.to_i}"
-      return false
-    end
-    # fail failure_message unless $?.to_i == 0
-  end
-
-  output
 end
