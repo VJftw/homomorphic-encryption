89b627578337c631f132ab9a661e1a609c176b67
VJ Patel <meetthevj@gmail.com>
2016-02-14 13:46:07 +0000

    Checking tag name before removing

--- backend/Rakefile.rb
+++ backend/Rakefile.rb
@@ -196,7 +196,7 @@ task :ci do
   prod_main_image.push do |chunk|
     j_chunk = JSON.parse(chunk)
     puts j_chunk
-    if j_chunk.include? 'aux'
+    if j_chunk.include? 'aux' and j_chunk['aux']['Tag'] == container_tag
       puts "Pushed: #{j_chunk['aux']['Tag']}"
       prod_main_image.remove
       prod_commit_image.remove
