46e88cdcfc6e4aca2daacea67ea9bba95a80edfe
VJ Patel <meetthevj@gmail.com>
2016-03-05 20:13:59 +0000

    Using MathJax

--- client/src/app/directive/mathjax.ts
+++ client/src/app/directive/mathjax.ts
@@ -1,16 +1,12 @@
 import {Directive, ViewContainerRef, TemplateRef} from 'angular2/core';
+import {ElementRef} from "angular2/core";
 
-
-@Directive({inputs: ['mathjax'], selector: '[mathjax]' })
+@Directive({selector: '[mathjax]' })
 export class MathJaxDirective {
 
   constructor(
-    private _viewContainer: ViewContainerRef,
-    private _templateRef: TemplateRef
-  ) { }
-
-  set mathjax(x) {
-    this._viewContainer.createEmbeddedView(this._templateRef);
-    //MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+  ) {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
   }
+
 }
