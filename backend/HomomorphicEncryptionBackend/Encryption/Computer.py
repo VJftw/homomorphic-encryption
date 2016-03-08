c8eb90284073b3d401d324919011d37447c74ca9
VJ Patel <meetthevj@gmail.com>
2016-03-08 12:22:36 +0000

    100% coverage in backend

--- backend/HomomorphicEncryptionBackend/Encryption/Computer.py
+++ backend/HomomorphicEncryptionBackend/Encryption/Computer.py
@@ -9,6 +9,9 @@ __author__ = "VJ Patel (vj@vjpatel.me)"
 
 
 class Computer:
+    """
+    Computer
+    """
 
     OPERATIONS = [
         "+",
@@ -22,8 +25,11 @@ class Computer:
 
     @inject(log_manager=LogManager)
     def __init__(self, log_manager):
+        """
+        :param LogManager log_manager:
+        :return:
+        """
         self.__logger = log_manager.get_logger("Computer")
-        pass
 
     def compute(self,
                 compute,
@@ -33,13 +39,10 @@ class Computer:
         return self.__do_command(compute, scope)
 
     def __do_command(self, command, scope):
-        if command == "generateRandomPrime()":
-            return self.__encryption_helper.generatePrime(16)
-        else:
-            return self.__calc_expression(command, scope)
+        return self.__calc_expression(command, scope)
 
     def __trim_brackets(self, expr):
-        if(expr[0] == "(" and expr[-1] == ")"):
+        if expr[0] == "(" and expr[-1] == ")":
             return expr[1:-1]
         else:
             return expr
@@ -50,41 +53,39 @@ class Computer:
 
         self.__logger.debug("Top operator: {0} at: {1}".format(top_op, top_op_loc))
 
-        aStr = self.__trim_brackets(expr[0:top_op_loc].strip())
-        bStr = self.__trim_brackets(expr[top_op_loc + 1:].strip())
+        a_str = self.__trim_brackets(expr[0:top_op_loc].strip())
+        b_str = self.__trim_brackets(expr[top_op_loc + 1:].strip())
 
-        self.__logger.debug("\taStr: {0}".format(aStr))
-        self.__logger.debug("\tbStr: {0}".format(bStr))
+        self.__logger.debug("\taStr: {0}".format(a_str))
+        self.__logger.debug("\tbStr: {0}".format(b_str))
 
+        c = None
         if top_op == "&":
-            parts = bStr.split(",")
-            bStr = parts[0]
-            cStr = parts[1]
-            self.__logger("\tbStr: {0}".format(bStr))
-            self.__logger("\tcStr: {0}".format(cStr))
+            parts = b_str.split(",")
+            b_str = parts[0]
+            c_str = parts[1]
+            self.__logger("\tbStr: {0}".format(b_str))
+            self.__logger("\tcStr: {0}".format(c_str))
 
-            # check if c requires more operations
-            if any(operator in cStr for operator in Computer.OPERATIONS):
-                c = self.__calc_expression(cStr, scope)
-            else:
-                c = self.__resolve_variable(cStr, scope)
+            # resolve c
+            c = self.__resolve_variable(c_str, scope)
 
         # check if a requires more operations
-        if any(operator in aStr for operator in Computer.OPERATIONS):
-            a = self.__calc_expression(aStr, scope)
+        if any(operator in a_str for operator in Computer.OPERATIONS):
+            a = self.__calc_expression(a_str, scope)
         else:
-            a = self.__resolve_variable(aStr, scope)
+            a = self.__resolve_variable(a_str, scope)
 
         # check if c requires more operations
-        if any(operator in bStr for operator in Computer.OPERATIONS):
-            b = self.__calc_expression(bStr, scope)
+        if any(operator in b_str for operator in Computer.OPERATIONS):
+            b = self.__calc_expression(b_str, scope)
         else:
-            b = self.__resolve_variable(bStr, scope)
+            b = self.__resolve_variable(b_str, scope)
 
         self.__logger.debug("\t\ta: {0}".format(a))
         self.__logger.debug("\t\tb: {0}".format(b))
 
-        if top_op == "&":
+        if top_op == "&" and c is not None:
             self.__logger.debug("\t\tc: {0}".format(c))
             self.__logger.debug(scope)
 
@@ -104,24 +105,23 @@ class Computer:
         if operator == "%":
             return a % b
         if operator == "$":
-            return modInv(a, b)
+            return self.__modinv(a, b)
         if operator == "&":
-            return a ^ b % c
+            return pow(a, b, c)
 
-    def egcd(a, b):
+    def __egcd(self, a, b):
         if a == 0:
-            return (b, 0, 1)
+            return b, 0, 1
         else:
-            g, y, x = egcd(b % a, a)
-            return (g, x - (b // a) * y, y)
+            g, y, x = self.__egcd(b % a, a)
+            return g, x - (b // a) * y, y
 
-    def modinv(a, m):
-        g, x, y = egcd(a, m)
+    def __modinv(self, a, m):
+        g, x, y = self.__egcd(a, m)
         if g != 1:
             raise Exception('modular inverse does not exist')
         else:
             return x % m
-
 
     def __find_top_operator_location(self, expr):
         bracket_counter = 0
@@ -148,8 +148,7 @@ class Computer:
         elif self.__is_int(v):
             return int(v)
         else:
-            raise Error("Cannot resolve variable: {0}".format(v))
-
+            raise Exception("Cannot resolve variable: {0}".format(v))
 
     def __is_int(self, string):
         try:
