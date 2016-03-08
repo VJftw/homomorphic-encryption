bc1b90b86e85e09575ba42d49eb01aa2fe22caca
VJ Patel <meetthevj@gmail.com>
2016-03-08 16:33:18 +0000

    added step for computation specs

--- client/src/app/model/computation/step.spec.ts
+++ client/src/app/model/computation/step.spec.ts
@@ -1,64 +1,83 @@
-//import {
-//  it
-//} from 'angular2/testing';
-//
-//import {Step} from './step';
-//import {BigInteger} from 'jsbn';
-//
-//describe('Step', () => {
-//
-//  let step: Step;
-//
-//  beforeEach(() => {
-//    step = new Step();
-//  });
-//
-//  it('should return the action', () => {
-//    expect(step.getAction())
-//      .toBeUndefined()
-//    ;
-//  });
-//
-//  it('should set the action', () => {
-//    expect(step.setAction('abcdef'))
-//      .toBe(step)
-//    ;
-//
-//    expect(step.getAction())
-//      .toEqual('abcdef')
-//    ;
-//  });
-//
-//  it('should return the result', () => {
-//    expect(step.getResult())
-//      .toBeUndefined()
-//    ;
-//  });
-//
-//  it('should set the result', () => {
-//    let r = new BigInteger('1');
-//
-//    expect(step.setResult(r))
-//      .toBe(step)
-//    ;
-//
-//    expect(step.getResult())
-//      .toBe(r)
-//    ;
-//  });
-//
-//  it('should return the timestamp', () => {
-//    expect(step.getTimestamp())
-//      .toBeUndefined()
-//    ;
-//  });
-//
-//  it('should set the timestamp', () => {
-//    let d = new Date();
-//
-//    expect(step.setTimestamp(d))
-//      .toBe(step)
-//    ;
-//  });
-//
-//});
+import {
+  it
+} from 'angular2/testing';
+
+import {Step} from './step';
+
+describe('Step', () => {
+
+  let step: Step;
+
+  beforeEach(() => {
+    step = new Step();
+  });
+
+  it('should return the encryption step', () => {
+    expect(step.getEncryptionStep())
+      .toBeUndefined()
+    ;
+  });
+
+  it('should set the encryption step', () => {
+    let encryptionStep = jasmine.createSpyObj('encryptionStep', ['']);
+
+    expect(step.setEncryptionStep(encryptionStep))
+      .toBe(step)
+    ;
+
+    expect(step.getEncryptionStep())
+      .toBe(encryptionStep)
+    ;
+  });
+
+  it('should return the variable', () => {
+    expect(step.getVariable())
+      .toBeUndefined()
+    ;
+  });
+
+  it('should set the variable', () => {
+    expect(step.setVariable('f'))
+      .toBe(step)
+    ;
+
+    expect(step.getVariable())
+      .toEqual('f')
+    ;
+  });
+
+  it('should return the calculation', () => {
+    expect(step.getCalculation())
+      .toBeUndefined()
+    ;
+  });
+
+  it('should set the calculation' ,() => {
+    expect(step.setCalculation('calc'))
+      .toBe(step)
+    ;
+
+    expect(step.getCalculation())
+      .toEqual('calc')
+    ;
+  });
+
+  it('should return the result', () => {
+    expect(step.getResult())
+      .toBeUndefined()
+    ;
+  });
+
+  it('should set the result', () => {
+    let result = jasmine.createSpyObj('result', ['']);
+
+    expect(step.setResult(result))
+      .toBe(step)
+    ;
+
+    expect(step.getResult())
+      .toBe(result)
+    ;
+  });
+
+});
